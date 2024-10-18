959a75be46400d2927a871690dab00f5875d16d3
Valerii <berezovskijvalera812@gmail.com>
2024-10-18 15:14:18 +0200

    operators { 10.18.24 }

--- TEMP/main.cpp
+++ TEMP/main.cpp
@@ -1,26 +1,66 @@
 #include<iostream>
-#include"Movie.h"
+#include<time.h>
 using namespace std;
 
+class Point 
+{
+	int x;
+	int y;
+public:
+	Point(): x(0), y(0) {}
+
+	Point(int x1, int y1) :x(x1), y(y1) { }
+
+	void Init() {
+		x = rand() % 10;
+		y = rand() % 10;
+	}
+
+	void Init(int x1, int y1) {
+		x = x1; y = y1;
+	}
+
+	void Output() {
+		cout << "X: " << x << "\tY: " << y << endl;
+	}
+
+	Point operator+(const Point& other) {
+		return Point(x + other.x, y + other.y);
+	}
+
+	Point operator-(const Point& other) {
+		return Point(x - other.x, y - other.y);
+	}
+	
+	Point operator*(const Point& other) {
+		return Point(x * other.x, y * other.y);
+	}
+	
+	Point operator/(const Point& other) {
+		return Point(x / other.x, y / other.y);
+	}
+
+	Point operator+(int a) {
+		return Point(x + a, y + a);
+	}
+
+	Point operator*(int a) {
+		return Point(x * a, y * a);
+	}
+
+	Point operator-(int a) {
+		return Point(x - a, y - a);
+	}
+
+	Point operator/(int a) {
+		return Point(x / a, y / a);
+	}
+};
 	
 int main()
 {
-	Movie* arr[3] = { 
-					  new Movie("aaa", "aaa", "aaa", "11.11.11", 1, 1),
-					  new Movie("bbb", "bbb", "bbb", "22.22.22", 2, 2),
-		              new Movie("ccc", "ccc", "ccc", "33.33.33", 3, 3)
-					};
-
-	for (int i = 0; i < 3; i++)
-	{
-		arr[i]->output();
-		cout << '\n';
-	}
-	cout << '\n';
-
-	for (int i = 0; i < 3; i++) {
-		delete arr[i];
-	}
-
-
+	Point a(2, 3);
+	Point b(2, 3);
+	Point c = a + 10;
+	c.Output();
 }
