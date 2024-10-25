a3a34c5e740ac04c7bf43ee95e94e7ef5880e2cf
Valerii <berezovskijvalera812@gmail.com>
2024-10-25 15:01:50 +0200

    оператор присваивания { 10/25/24}

--- TEMP/main.cpp
+++ TEMP/main.cpp
@@ -1,173 +1,63 @@
-/*//#include <iostream>
-using namespace std;
-
-class Fraction {
-private:
-	int numerator;
-	int denominator;
-public:
-	void Input(int x, unsigned y) {
-		numerator = x;
-		denominator = y;
-	}
-	void Input() {
-		numerator = rand() % 100;
-		denominator = rand() % 100 + 1;
-	}
-	void print() {
-		cout << numerator << '/' << denominator << endl;
-	}
-	Fraction(int numerator_ = 0, int denominator_ = 1) : numerator(numerator_), denominator(denominator_) {}
-
-	int getNumerator() const { return numerator; }
-	int getDenominator() const { return denominator; }
-
-	int commonDenominator(const Fraction& frac1, const Fraction& frac2) {
-		int d1 = frac1.denominator;
-		int d2 = frac2.denominator;
-
-		while (d1 != d2) {
-			if     (d1 < d2){ d1 += frac1.denominator; }
-			else if(d2 < d1){ d2 += frac2.denominator; }
-		}
-		return d1;
-	}
-	Fraction operator+(const Fraction& other){
-		if (this->denominator == other.denominator) {
-			return Fraction(this->numerator + other.numerator, this->denominator);
-		}
-		const int cdenominator = commonDenominator(*this, other);
-		return Fraction(this->numerator * (cdenominator / this->denominator) + other.numerator * (cdenominator / other.denominator), cdenominator);
-	}
-	Fraction operator-(const Fraction& other) {
-		if (this->denominator == other.denominator) {
-			return Fraction(this->numerator - other.numerator, this->denominator);
-		}
-		const int cdenominator = commonDenominator(*this, other);
-		return Fraction(this->numerator * (cdenominator / this->denominator) - other.numerator * (cdenominator / other.denominator), cdenominator);
-	}
-	Fraction operator*(const Fraction& other) {
-		return Fraction(this->numerator * other.numerator, this->denominator * other.denominator);
-	}
-	Fraction operator/(const Fraction& other) {
-		return Fraction(this->numerator * other.denominator, this->denominator * other.numerator);
-	}
-	Fraction operator++() {
-		numerator += denominator;
-		return *this;
-	}
-	Fraction operator++(int) {
-		Fraction temp(*this);
-		numerator += denominator;
-		return temp;
-	}
-	Fraction operator--() {
-		numerator -= denominator;
-		return *this;
-	}
-	Fraction operator--(int) {
-		Fraction temp(*this);
-		numerator -= denominator;
-		return temp;
-	}
-
-};
-
-int main() {
-	srand(time(NULL));
-
-	Fraction frac (10, 2);
-	frac.print();
-
-	Fraction frac2(2,3);
-	frac2.print();
-
-	Fraction c = ++frac;
-	c.print();
-
-}
-*/
-
 #include<iostream>
+#include<Windows.h>
 using namespace std;
 
-class Circle
+class Student
 {
-	int r;
+	char* name;
+	int age;
 public:
-	Circle() : r(0) { }
-	Circle(int r) : r(r) { }
-	void Output() {
-		cout << "r: " << r << endl; }
+	////  prototype 
+	Student();
+	Student(const char*, int); // construct by 2 param
+	Student(const Student& obj)
+		: age(obj.age)
+	{
+		name = new char[strlen(obj.name) + 1];
+		strcpy_s(name, strlen(obj.name) + 1, obj.name);
+	}
 
-	int getLenght() { 
-		return 2 * 3.14 * r; }
-
-	Circle operator+(Circle& b) {
-		Circle temp;
-		temp.r = this->r + b.r;
-		return temp;
-	}
-	Circle operator+(int a) {
-		Circle temp(r + a);
-		return temp;
-	}
-	bool operator>(Circle& other) {
-		if (other.getLenght() > this->getLenght()) { return true; }
-		return false;
-	}
-	bool operator<(Circle& other) {
-		if (other.getLenght() < this->getLenght()) { return true; }
-		return false;
-	}
-	bool operator ==(Circle& other) {
-		if (other.r == this->r) { return true; }
-		return false;
-	}
-	bool operator !=(Circle& other) {
-		if (*this == other) { return false; }
-		return true;
-	}
-	Circle& operator+=(int a) {
-		r += a;
-		return *this;
-	}
-	Circle& operator-=(int a) {
-		if (r > a) {
-			r -= a;
+	Student operator=(Student& other) {
+		if (this != &other) {
+			if (name != nullptr) { delete[] name; }
+			name = new char[strlen(other.name) + 1];
+			strcpy_s(name, strlen(other.name) + 1, other.name);
+			age = other.age;
 		}
 		return *this;
 	}
-	Circle& operator*=(int a) {
-		r *= a;
-		return *this;
-	}
-	Circle& operator/=(int a) {
-		r /= a;
-		return *this;
-	}
-	Circle operator++() {
-		r += 1;
-		return *this;
-	}
-	Circle operator++(int) {
-		Circle temp(*this);
-		r += 1;
-		return temp;
-	}
-	Circle operator--() {
-		if (r > 1) {
-			r -= 1;
-		}		return *this;
-	}
-	Circle operator--(int) {
-		Circle temp(*this);
-		r -= 1;
-		return temp;
-	}
+	void Output();
+	~Student();
+
 };
+Student::Student()
+{
+	name = nullptr;
+	age = 0;
+}
+Student::Student(const char* Name, int Age)
+{
+	name = new char[strlen(Name) + 1];
+	strcpy_s(name, strlen(Name) + 1, Name);
+	age = Age;
+}
+void Student::Output()
+{
+	cout << "Name: " << name << endl
+		<< "Age: " << age << endl << endl;
+}
+Student::~Student()
+{
+	if (name != nullptr)
+	{
+		delete[] name;
+	}
+}
 
 int main()
 {
-
+	Student obj1("Oleg", 21);
+	Student obj2("Og", 21);
+	obj1 = obj2;
+	obj1.Output();
 }
