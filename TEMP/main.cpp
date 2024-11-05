8df97cf43874c231e947ea0089b548876d7c1269
Valerii <berezovskijvalera812@gmail.com>
2024-11-05 17:52:32 +0100

    шаблоные функции { 11.5.24 }

--- TEMP/main.cpp
+++ TEMP/main.cpp
@@ -1,210 +1,56 @@
-#include<iostream>
+﻿#include<iostream>
 using namespace std;
 
-class Point
-{
-	int x;
-	int y;
-public:
-	Point()  // construct by default
-	{
-		cout << "Construct\n";
-		x = y = 0;
-	}
-	Point(int x1, int y1)   // Construct by 2 param
-	{
-		cout << "Construct by 2 param\n";
-		x = x1;
-		y = y1;
-	}
-	void Init()
-	{
-		x = rand() % 10;
-		y = rand() % 10;
-	}
-	void Init(int x1, int y1)
-	{
-		x = x1;
-		y = y1;
-	}
-	void Output()
-	{
-		cout << "X: " << x << "\tY: " << y << endl;
-	}
-
-	int GetX()
-	{
-		return x;
-	}
-	int GetY()
-	{
-		return y;
-	}
-	void SetX(int a)
-	{
-		x = a;
-	}
-	void SetY(int a)
-	{
-		y = a;
-	}
-};
-
-Point operator+(int a, Point b) {
-	Point rez;
-	rez.SetX(b.GetX() + a);
-	rez.SetY(b.GetY() + a);
-	return rez;
+template<class T>
+auto maxElementArray(T arr[], int size) {
+	T max = arr[0];
+	for (int i = 0; i < size; ++i)
+		max = std::max(arr[i], max);
+	return max;
 }
 
-Point operator+(Point b, int a) {
-	Point rez;
-	rez.SetX(b.GetX() + a);
-	rez.SetY(b.GetY() + a);
-	return rez;
+template<class T>
+auto maxElementArray2D(T(*array)[2], int size) {
+	T max = array[0][0];
+	for (int i = 0; i < 4; ++i)
+		for (int j = 0; j < size; ++j)
+			max = std::max(array[i][j], max);
+	return max;
 }
 
-int operator+(Point a, Point b) {
-	return a.GetX() + b.GetX() * a.GetY() + b.GetY();
+template<class T>
+auto maxElementArray3D(T array[][3][3], int row, int col, int dpt) {
+	T max = array[0][0][0];
+	for (int i = 0; i < row; ++i)
+		for (int j = 0; j < col; ++j)
+			for (int k = 0; k < dpt; ++k)
+				max = std::max(array[i][j][k], max);
+	return max;
 }
 
-Point operator-(int a, Point b) {
-	Point rez;
-	rez.SetX(b.GetX() - a);
-	rez.SetY(b.GetY() - a);
-	return rez;
+template<class T>
+auto maxOfTwo(T first, T second) {
+	return std::max(first, second);
 }
 
-Point operator-(Point b, int a) {
-	Point rez;
-	rez.SetX(b.GetX() - a);
-	rez.SetY(b.GetY() - a);
-	return rez;
-}
-
-Point operator*(int a, Point b) {
-	Point rez;
-	rez.SetX(b.GetX() * a);
-	rez.SetY(b.GetY() * a);
-	return rez;
-}
-
-Point operator*(Point b, int a) {
-	Point rez;
-	rez.SetX(b.GetX() * a);
-	rez.SetY(b.GetY() * a);
-	return rez;
-}
-
-Point operator/(int a, Point b) {
-	Point rez;
-	rez.SetX(b.GetX() / a);
-	rez.SetY(b.GetY() / a);
-	return rez;
-}
-
-Point operator/(Point b, int a) {
-	Point rez;
-	rez.SetX(b.GetX() / a);
-	rez.SetY(b.GetY() / a);
-	return rez;
-}
-
-
-
-Point operator++(Point& a) {
-	a.SetX(a.GetX() + 10);
-	a.SetY(a.GetY() + 10);
-	return a;
-
-}
-Point operator++(Point& a, int) {
-	Point temp = a;
-	a.SetX(a.GetX() + 10);
-	a.SetY(a.GetY() + 10);
-	return temp;
-}
-
-
-Point operator--(Point& a) {
-	a.SetX(a.GetX() - 10);
-	a.SetY(a.GetY() - 10);
-	return a;
-
-}
-Point operator--(Point& a, int) {
-	Point temp = a;
-	a.SetX(a.GetX() - 10);
-	a.SetY(a.GetY() - 10);
-	return temp;
-}
-
-Point& operator+=(Point& a, Point& b) {
-	a.SetX(a.GetX() + b.GetX());
-	a.SetY(a.GetY() + b.GetY());
-	return a;
-}
-
-Point& operator+=(Point& a, int b) {
-	a.SetX(a.GetX() + b);
-	a.SetY(a.GetY() + b);
-	return a;
-}
-
-
-Point& operator-=(Point& a, Point& b) {
-	a.SetX(a.GetX() + b.GetX());
-	a.SetY(a.GetY() + b.GetY());
-	return a;
-}
-
-Point& operator-=(Point& a, int b) {
-	a.SetX(a.GetX() - b);
-	a.SetY(a.GetY() - b);
-	return a;
-}
-
-Point& operator*=(Point& a, Point& b) {
-	a.SetX(a.GetX() * b.GetX());
-	a.SetY(a.GetY() * b.GetY());
-	return a;
-}
-
-Point& operator*=(Point& a, int b) {
-	a.SetX(a.GetX() * b);
-	a.SetY(a.GetY() * b);
-	return a;
-}
-
-Point& operator/=(Point& a, Point& b) {
-	a.SetX(a.GetX() / b.GetX());
-	a.SetY(a.GetY() / b.GetY());
-	return a;
-}
-
-Point& operator/=(Point& a, int b) {
-	a.SetX(a.GetX() / b);
-	a.SetY(a.GetY() / b);
-	return a;
+template<class T>
+auto maxOfThree(T first, T second, T third) {
+	return std::max(first, std::max(second, third));
 }
 
 int main()
 {
-	srand(unsigned(time(0)));
+	char arr[4][2]{ {'c','v'}, {'C','z'}, {'1','l'}, {'x','x'} };
 
-	Point a(1, 2);
-	Point b(3, 4);
+	char arr2[5]{ 'c','v','v','z','s' };
 
-	Point q = 100 - a;
-	q = a * 10;
-	q = 100 / a;
+	int arr3[3][3][3] = {
+		{ { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } },
+		{ { 10, 11, 12 }, { 13, 14, 15 }, { 16, 17, 18 } },
+		{ { 19, 20, 21 }, { 22, 99, 24 }, { 25, 26, 27 } }
+	};
 
-	int res = a + b;
 
-	Point t = --a;
-	Point d = a--;
-
-	a += 10;
-	a += b;
-
+	cout << maxElementArray3D(arr3, 3,3,3) << endl;
+	cout << maxOfThree(2, 3, 10) << endl;
 }
